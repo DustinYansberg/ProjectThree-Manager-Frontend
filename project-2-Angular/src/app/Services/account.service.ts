83d2ee64497a189832e95afb3461e0ceac7c2db8
Jared Mai <jaredmai2@gmail.com>
2024-08-20 04:04:08 -0400

    Update account.service.ts

--- project-2-Angular/src/app/Services/account.service.ts
+++ project-2-Angular/src/app/Services/account.service.ts
@@ -9,26 +9,26 @@ export class AccountService {
 
   constructor(private http: HttpClient, private authService: AuthService) { }
 
-  url: String = 'http://localhost:3000/';
+  url: String = 'http://localhost:8081/';
 
   private getHeaders() {
     return this.authService.getHeaders();
   }
 
   getAllAccounts() {
-    return this.http.get(this.url + 'api/accounts', { headers: this.getHeaders(), observe: 'response' });
+    return this.http.get(this.url + 'account', { headers: this.getHeaders(), observe: 'response' });
   }
 
   getAccountById(id: string) {
-    return this.http.get(this.url + 'api/accounts/' + id, { headers: this.getHeaders(), observe: 'response' });
+    return this.http.get(this.url + 'account/' + id, { headers: this.getHeaders(), observe: 'response' });
   }
 
   createAccount(accountDetails: any) {
-    return this.http.post(this.url + 'api/accounts', accountDetails, { headers: this.getHeaders(), observe: 'response' });
+    return this.http.post(this.url + 'account', accountDetails, { headers: this.getHeaders(), observe: 'response' });
   }
 
   deleteAccount(id: string) {
-    return this.http.delete(this.url + 'api/accounts/' + id, { headers: this.getHeaders(), observe: 'response' });
+    return this.http.delete(this.url + 'account/' + id, { headers: this.getHeaders(), observe: 'response' });
   }
 
 }
