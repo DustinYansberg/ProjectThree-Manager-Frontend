ac2a3ea34f37f6491b9618efa1f96e08b5cb0c4a
Yuri Beneche <yurisenai742@gmail.com>
2024-08-27 12:45:01 -0400

    remove auth service

--- project-2-Angular/src/app/Services/appointment.service.ts
+++ project-2-Angular/src/app/Services/appointment.service.ts
@@ -1,30 +1,27 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Appointment } from '../Models/appointment';
-import { AuthService } from './auth.service';
+
 
 @Injectable()
 export class AppointmentService {
 
-  constructor(private http: HttpClient, private authService: AuthService) { }
+  constructor(private http: HttpClient) { }
 
-  //url: String = 'http://4.156.40.62:9001/appointment/';
-  url: String = 'http://localhost:8085/appointment/';
+  url: String = 'http://4.156.40.62:9001/appointment/';
+  // url: String = 'http://localhost:8085/appointment/';
 
-  private getHeaders() {
-    return this.authService.getHeaders();
-  }
 
   getAppointmentByOrganizerId(id: string) {
-    return this.http.get(this.url + 'getByOrganizerId/' + id, { headers: this.getHeaders(), observe: 'response' });
+    return this.http.get(this.url + 'getByOrganizerId/' + id, {  observe: 'response' });
   }
 
   createAppointment(appointment: Appointment) {
-    return this.http.post(this.url + 'create', appointment, { headers: this.getHeaders(), observe: 'response' });
+    return this.http.post(this.url + 'create', appointment, { observe: 'response' });
   }
 
   deleteAppointment(id : number) {
-    return this.http.put(this.url + 'delete/' + id, { headers: this.getHeaders(), observe: 'response' });
+    return this.http.put(this.url + 'delete/' + id, { observe: 'response' });
   }
 
   
