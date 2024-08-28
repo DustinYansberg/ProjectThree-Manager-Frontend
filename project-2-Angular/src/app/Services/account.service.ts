b726e143a807a55e799c3ad9aec26dbe8e58bff2
Jared Mai <jaredmai2@gmail.com>
2024-08-28 12:35:29 -0400

    Accounts Fix

--- project-2-Angular/src/app/Services/account.service.ts
+++ project-2-Angular/src/app/Services/account.service.ts
@@ -6,15 +6,15 @@ export class AccountService {
 
   constructor(private http: HttpClient) { }
 
-  // url: String = 'http://localhost:8082/';
-  url: String = 'http://4.156.40.62:9001/'
+   url: String = 'http://localhost:8082/';
+  //url: String = 'http://4.156.40.62:9001/'
 
   getAllAccounts() {
     return this.http.get(this.url + 'account', {observe: 'response' });
   }
 
   getAccountById(id: string) {
-    return this.http.get(this.url + 'account/' + id, { observe: 'response' });
+    return this.http.get(this.url + 'account/identity/' + id, { observe: 'response' });
   }
 
   createAccount(accountDetails: any) {
